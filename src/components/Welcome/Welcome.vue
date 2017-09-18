129bc43571ba01dc2e154679a1fda25a68db32ec
burak <tknbrk@gmail.com>
2017-09-18 07:59:38 +0200

    fixed minor issues

--- src/components/Welcome/Welcome.vue
+++ src/components/Welcome/Welcome.vue
@@ -7,88 +7,79 @@
     </header>
     
     <div class="container">
-	    <section>
-	      <div class="row align-items-center">
-	      	<div id="description" class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-	          <div class="text-center mt-3"><h2><b>Welcome to VueNews</b></h2></div>
-	          <div class="p-4 mt-3 text-left">
-	            <h2 class="display-5"><b>For those who seek getting top latest news about their interests.</b></h2>
-	            <p class="mt-3">
-	              <p>VueNews is a <i>Hackernews</i> client which is created for people want to follow only specific topics they are interested in.</p>
-	            </p>
+      <div class="row align-items-center">
+      	<div id="description" class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+          <div class="text-center mt-3"><h2><b>Welcome to VueNews</b></h2></div>
+          <div class="mt-3 text-left">
+            <h2 class="display-5 detail"><b>For those who seek getting top latest news about their interests.</b></h2>
+            <p>VueNews is a <i>Hackernews</i> client which is created for people want to follow only specific topics they are interested in.</p>
+          </div>
+          <div class="row mt-3 clearfix">
+				    <div class="col-xs-6">
+	        		<router-link to="./authentication#register" class="btn btn-sm btn-block btn-info">Sign Up</router-link>
+				    </div>
+				    <div class="col-xs-6">
+	        		<router-link to="./authentication#login" class="btn btn-sm btn-block btn-info">Sign In</router-link>
+				    </div>
+	        </div>
+        </div>
+
+        <div class="animation content-wrapper col-xs-12 col-sm-12 col-md-12 col-lg-6">
+	        <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+	        	<div class="row align-items-center text-center">
+          		<div class="icons">
+	            	<div class="vn-brand-animate">
+	            		<i class="fa fa-automobile"></i>
+		            	<i class="fa fa-camera slide"></i>
+		            	<i class="fa fa-gamepad"></i>
+	            	</div>
+	            	<div class="vn-brand-animate">
+	            		<i class="fa fa-music"></i>
+		            	<i class="fa fa-language slide"></i>
+		            	<i class="fa fa-cube"></i>
+	            	</div>
+	            	<div class="vn-brand-animate">
+	            		<i class="fa fa-code slide"></i>
+		            	<i class="fa fa-fire"></i>
+		            	<i class="fa fa-globe"></i>
+	            	</div>
+	            	<div class="vn-brand-animate">
+	            		<i class="fa fa-glass"></i>
+		            	<i class="fa fa-gamepad"></i>
+		            	<i class="fa fa-database"></i>
+	            	</div>
+	            	<div class="vn-brand-animate">
+	            		<i class="fa fa-legal"></i>
+		            	<i class="fa fa-paw"></i>
+		            	<i class="fa fa-rocket slide"></i>
+	            	</div>
+	            </div>
 	          </div>
-	          <div class="row justify-content-md-center">
-					    <div class="col-6 col-md-auto">
-		        		<router-link to="./authentication#register" class="btn btn-md btn-block btn-info">Sign Up</router-link>
-					    </div>
-					    <div class="col-6 col-md-auto">
-		        		<router-link to="./authentication#login" class="btn btn-md btn-block btn-info">Sign In</router-link>
-					    </div>
-		        </div>
+	        </div>
+	      
+
+	        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+	        	<img id="picture" class="img-fluid" src="../../assets/hackernews.jpg" alt="">
 	        </div>
 
-	        <div class="animation content-wrapper col-xs-12 col-sm-12 col-md-12 col-lg-6">
-		        <div class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
-		        	<div class="row align-items-center text-center">
-	          		<div class="icons">
-		            	<div class="vn-brand-animate">
-		            		<i class="fa fa-automobile"></i>
-			            	<i class="fa fa-camera slide"></i>
-			            	<i class="fa fa-gamepad"></i>
-		            	</div>
-		            	<div class="vn-brand-animate">
-		            		<i class="fa fa-heart"></i>
-			            	<i class="fa fa-institution"></i>
-			            	<i class="fa fa-magnet"></i>
-		            	</div>
-		            	<div class="vn-brand-animate">
-		            		<i class="fa fa-music"></i>
-			            	<i class="fa fa-language slide"></i>
-			            	<i class="fa fa-cube"></i>
-		            	</div>
-		            	<div class="vn-brand-animate">
-		            		<i class="fa fa-code slide"></i>
-			            	<i class="fa fa-fire"></i>
-			            	<i class="fa fa-globe"></i>
-		            	</div>
-		            	<div class="vn-brand-animate">
-		            		<i class="fa fa-glass"></i>
-			            	<i class="fa fa-gamepad"></i>
-			            	<i class="fa fa-database"></i>
-		            	</div>
-		            	<div class="vn-brand-animate">
-		            		<i class="fa fa-legal"></i>
-			            	<i class="fa fa-paw"></i>
-			            	<i class="fa fa-rocket slide"></i>
-		            	</div>
-		            </div>
-		          </div>
-		        </div>
-		      
-
-		        <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-		        	<img id="picture" class="img-fluid" src="../../assets/hackernews.jpg" alt="">
-		        </div>
-
-		        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
-		        	<div class="icons">
-			        	<div class="vn-brand-animate">
-		            	<i class="fa fa-2x fa-hacker-news"></i>
-		          	</div>
-		          	<div class="vn-brand-animate">
-		            	<i class="fa fa-2x fa-hacker-news"></i>
-		          	</div>
-		          	<div class="vn-brand-animate">
-		            	<i class="fa fa-2x fa-hacker-news"></i>
-		          	</div>
-		          	<div class="vn-brand-animate">
-		            	<i class="fa fa-2x fa-hacker-news"></i>
-		          	</div>
-		          </div>
-		        </div>
-		      </div>
+	        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
+	        	<div class="icons">
+		        	<div class="vn-brand-animate">
+	            	<i class="fa fa-2x fa-hacker-news"></i>
+	          	</div>
+	          	<div class="vn-brand-animate">
+	            	<i class="fa fa-2x fa-hacker-news"></i>
+	          	</div>
+	          	<div class="vn-brand-animate">
+	            	<i class="fa fa-2x fa-hacker-news"></i>
+	          	</div>
+	          	<div class="vn-brand-animate">
+	            	<i class="fa fa-2x fa-hacker-news"></i>
+	          	</div>
+	          </div>
+	        </div>
 	      </div>
-	    </section>
+      </div>
 	  </div>
 	  <!-- Footer -->
 	  <footer class="bottom ">
